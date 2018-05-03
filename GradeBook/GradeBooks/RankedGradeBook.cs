8f74d7c39a94ea3844735e072ceac00eaf4ae47f
KyleMcSheffery <36888419+KyleMcSheffery@users.noreply.github.com>
2018-05-03 16:01:41 +0100

    Override stats

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -44,28 +44,18 @@ namespace GradeBook.GradeBooks
                 return 'D';
             }
 
-
-
-
-
-            /*else if (averageGrade >= Students[Students.Count / 5 * 4].AverageGrade)
-            {
-                return 'A';
-            }
-            else if (averageGrade >= Students[Students.Count / 5 * 3].AverageGrade)
-            {
-                return 'B';
-            }
-            else if (averageGrade >= Students[Students.Count / 5 * 2].AverageGrade)
-            {
-                return 'C';
-            }
-            else if (averageGrade >= Students[Students.Count / 5].AverageGrade)
-            {
-                return 'D';
-            }*/
-
             return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
         }
 
     }

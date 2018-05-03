251e856c37dbd260d8607799663e1124f22a0d7f
KyleMcSheffery <36888419+KyleMcSheffery@users.noreply.github.com>
2018-05-03 15:58:39 +0100

    Ranked letter grades

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,61 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
+        public override char GetLetterGrade(double averageGrade)
+        {
+            double[] avgGrades = new double[Students.Count];
+
+            for (int i = 0; i < Students.Count; i++)
+            {
+                avgGrades[i] = Students[i].AverageGrade;
+            }
+
+            Array.Sort(avgGrades);
+                
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least five students.");
+            }
+            else if (averageGrade >= avgGrades[Students.Count / 5 * 4])
+            {
+                return 'A';
+            }
+            else if (averageGrade >= avgGrades[Students.Count / 5 * 3])
+            {
+                return 'B';
+            }
+            else if (averageGrade >= avgGrades[Students.Count / 5 * 2])
+            {
+                return 'C';
+            }
+            else if (averageGrade >= avgGrades[Students.Count / 5])
+            {
+                return 'D';
+            }
+
+
+
+
+
+            /*else if (averageGrade >= Students[Students.Count / 5 * 4].AverageGrade)
+            {
+                return 'A';
+            }
+            else if (averageGrade >= Students[Students.Count / 5 * 3].AverageGrade)
+            {
+                return 'B';
+            }
+            else if (averageGrade >= Students[Students.Count / 5 * 2].AverageGrade)
+            {
+                return 'C';
+            }
+            else if (averageGrade >= Students[Students.Count / 5].AverageGrade)
+            {
+                return 'D';
+            }*/
+
+            return 'F';
+        }
+
     }
 }
